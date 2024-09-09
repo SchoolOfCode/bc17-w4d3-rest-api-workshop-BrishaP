dd75e553cb1306cd9dda4617521b6a23e0736f6a
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-09-09 14:05:48 +0100

    added completed tasks

--- app.js
+++ app.js
@@ -57,4 +57,101 @@ listen to requests at the appropriate path. */
 /* Write the request handler to perform the action and return the data from the function updateAstronautById. Have this handler 
 listen to requests at the appropriate path. */
 
+
+
+// Task 1
+app.get('/astronauts', async (req, res) => {
+  try {
+    const astronauts = await getAstronauts();
+    res.json({
+      success: true,
+      payload: astronauts
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      payload: error.message
+    });
+  }
+});
+
+// Task 2
+app.post('/astronauts', async (req, res) => {
+  try {
+    const newAstronaut = await createAstronaut(req.body);
+    res.json({
+      success: true,
+      payload: newAstronaut
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      payload: error.message
+    });
+  }
+});
+
+// Task 3
+app.get('/astronauts/:id', async (req, res) => {
+  try {
+    const astronaut = await getAstronautById(req.params.id);
+    res.json({
+      success: true,
+      payload: astronaut
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      payload: error.message
+    });
+  }
+});
+
+// Task 4
+app.put('/astronauts/:id', async (req, res) => {
+  try {
+    const updatedAstronaut = await replaceAstronautById(req.params.id, req.body);
+    res.json({
+      success: true,
+      payload: updatedAstronaut
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      payload: error.message
+    });
+  }
+});
+
+// Task 5
+app.delete('/astronauts/:id', async (req, res) => {
+  try {
+    const deletedAstronaut = await deleteAstronautById(req.params.id);
+    res.json({
+      success: true,
+      payload: deletedAstronaut
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      payload: error.message
+    });
+  }
+});
+
+// Task 6
+app.patch('/astronauts/:id', async (req, res) => {
+  try {
+    const updatedAstronaut = await updateAstronautById(req.params.id, req.body);
+    res.json({
+      success: true,
+      payload: updatedAstronaut
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      payload: error.message
+    });
+  }
+});
 export default app;
